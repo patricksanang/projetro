aad0c7d8dc382bca4cc231fd64cf701cd088a361
mma48 <marc.mbida@polytechnique.cm>
2016-04-30 10:39:51 +0100

    modification interface

--- Web/Frontend/header.php
+++ Web/Frontend/header.php
@@ -4,6 +4,6 @@
 		<h3 class="msg_accueil1"> Baladez vous dans la ville en fonction de votre budget... et de votre temps...</h3>
 	</div>
 	<div class="msg2">
-		<a class="btn btn-primary btn1 btn-primary1 animated3" href="#option"> Allez-y maintenant</a>
+		<a class="btn btn-primary btn1 btn-primary1" href="#option"> Allez-y maintenant</a>
 	</div>
 </section>
